5c35bcb5adb3ba84334b94a459fee1826390ac67
mecca <mecca.benson@gmail.com>
2026-08-19 13:49:07 -0400

    Add discussions page

--- java/src/main/java/com/zipcode/stardust/controller/ForumController.java
+++ java/src/main/java/com/zipcode/stardust/controller/ForumController.java
@@ -1,19 +1,28 @@
 package com.zipcode.stardust.controller;
 
-import com.zipcode.stardust.model.*;
-import com.zipcode.stardust.repository.*;
-import com.zipcode.stardust.service.ForumService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import com.zipcode.stardust.model.Comment;
+import com.zipcode.stardust.model.Post;
+import com.zipcode.stardust.model.Subforum;
+import com.zipcode.stardust.model.User;
+import com.zipcode.stardust.repository.CommentRepository;
+import com.zipcode.stardust.repository.PostRepository;
+import com.zipcode.stardust.repository.SubforumRepository;
+import com.zipcode.stardust.repository.UserRepository;
+import com.zipcode.stardust.service.ForumService;
 
 @Controller
 public class ForumController {
@@ -56,6 +65,12 @@ public class ForumController {
         List<Subforum> topLevel = subforumRepository.findByParentIsNull();
         model.addAttribute("subforums", topLevel);
         return "subforums";
+    }
+    
+    @GetMapping("/discussions")
+    public String discussions(Model model, Authentication auth) {
+    addCommonAttributes(model, auth);
+    return "discussions";
     }
 
     @GetMapping("/subforum")
